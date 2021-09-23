b704e20e95b9f2d608562aafe67b42f3a75cc870
Harsha Vardhan B <80052719+h8rsha@users.noreply.github.com>
2021-09-23 20:04:42 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    print("Welcome to the Band Name Generator.")
+    city_name = input("What's name of the city you grew up in?\n")
+    pet_name = input("What's your pet name?\n")
+    print("Your band name could be " + city_name + " " + pet_name)
+
